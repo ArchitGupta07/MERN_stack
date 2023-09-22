05c834dfc67fe353096146ad59e3ca3bb58ab2ce
Archit Gupta <gupta.archit01@gmail.com>
2023-09-22 11:41:55 +0530

    Added

--- nextapp/prisma/migrations/20230922060242_init/migration.sql
+++ nextapp/prisma/migrations/20230922060242_init/migration.sql
@@ -6,3 +6,12 @@ CREATE TABLE "Todo" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated" DATETIME NOT NULL
 );
+
+-- CreateTable
+CREATE TABLE "Podo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "case_id" INTEGER NOT NULL,
+    "lawyer" TEXT NOT NULL,
+    "court" TEXT NOT NULL
+);
