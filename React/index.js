a9cb609a4b813c87109e54a76fb826bc7bfa2525
Archit Gupta <gupta.archit01@gmail.com>
2023-09-25 18:50:35 +0530

    Update index.js

--- React/index.js
+++ React/index.js
@@ -1,3 +1,14 @@
 ReactDOM.render(<h1>Hello Everyone</h1>, document.getElementById('root')) //first argument is to tell what to put, and second argument is to tell where to put
 
 
+ReactDOM.render(<p>My name is Archit</p>, document.getElementById('root'))
+//note that when you use ReactDOM.render, it replaces the content of the target element with the new JSX element you provide.
+// In the end, only the content rendered by the second ReactDOM.render call will be visible because it overwrites the content rendered by the first call.
+
+ReactDOM.render(
+    <div>
+      <h1>Hello Everyone</h1>
+      <p>My name is Archit</p>
+    </div>,
+    document.getElementById('root')
+  ); // to render both of them 
