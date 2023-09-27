6d957626cd5ea8114cf8bf5e9d8115effda4563b
Archit Gupta <gupta.archit01@gmail.com>
2023-09-27 11:49:42 +0530

    Added

--- React/Tut2_create_app/my-app/src/components/Main.js
+++ React/Tut2_create_app/my-app/src/components/Main.js
@@ -1,6 +1,18 @@
 import React from "react"
 
 export default function Main() {
-    return <h1>Main component</h1>
+    return (
+        <main>
+
+            <h1 className="main--title">Fun Facts about react</h1>
+            <ul className="main--facts">
+                <li>released</li>
+                <li>managed by facebook</li>
+
+
+            </ul>
+
+        </main>
+    )
 
 }
--- React/Tut2_create_app/my-app/src/components/Navbar.js
+++ React/Tut2_create_app/my-app/src/components/Navbar.js
@@ -1,6 +1,12 @@
 import React from "react"
 
 export default function Navbar() {
-    return <h1>Navbar component</h1>
+    return (
+        <nav>
+        <img  className="nav--icon" src="" alt="" />
+        <h3 className="nav--logo-text">React Facts</h3>
+        <h4 className="nav--title">React course-project 1</h4>
+        </nav>
+    )
 
 }
