c5157108982c5d31cb291055b357981c86941cbb
Archit Gupta <gupta.archit01@gmail.com>
2023-10-01 10:54:34 +0530

    Create card_data.js

---
+++ React/Tut2_create_app/my-app/src/components/card_data.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        id:1,
+        title: "title hai bhai",
+        description:"description hai bhai",
+        price:125,
+        coverImg:"",
+        stats:{
+            rating:5.0,
+            reviewCount:30
+
+        },
+        location:"Online",
+        openSpots:27,
+
+
+    },
+    
+    {
+        id:1,
+        title: "dusra title hai bhai",
+        description:"dusra description hai bhai",
+        price:200,
+        coverImg:"",
+        stats:{
+            rating:5.0,
+            reviewCount:60
+
+        },
+        location:"offline",
+        openSpots:100,
+    }
+]
