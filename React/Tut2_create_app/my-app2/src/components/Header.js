f95d6ea28dc3876e6e59e07dd62547c025d73bae
Archit Gupta <gupta.archit01@gmail.com>
2023-10-02 11:18:22 +0530

    Added

---
+++ React/Tut2_create_app/my-app2/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(params) {
+  return(
+    <header>
+    <img src="" alt="" />
+    <h2>Header component</h2>
+    <h4>React Course - Project3</h4>
+    </header>
+  )  
+};
