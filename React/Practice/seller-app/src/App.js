69110ab9c6cdb8496629143b93e17d0449a52bb5
Archit Gupta <gupta.archit01@gmail.com>
2023-10-03 12:49:26 +0530

    Added

--- React/Practice/seller-app/src/App.js
+++ React/Practice/seller-app/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header/>
+      <div className='category--buttons'>
+
+        <button className='category--newyork'>New York</button>
+        <button className='category--mumbai'>Mumbai</button>
+        <button className='category--paris'>Paris</button>
+        <button className='category--london'>London</button>
+
+      </div>
     </div>
   );
 }
---
+++ React/Practice/seller-app/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header(params) {
+
+    return(
+        <header>
+        <div className="header">
+
+            <h2>Featured Listed Property</h2>
+            <h4>Real state can be bought, sold , leased, or rented, and can be a valuable investment opportunity. The value of real estate can be ...</h4>
+        </div>
+        </header>
+    )
+    
+};
