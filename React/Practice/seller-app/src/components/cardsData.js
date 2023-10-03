7b7feb206218532706bc19ca1e07ad46187f0531
Archit Gupta <gupta.archit01@gmail.com>
2023-10-03 13:15:26 +0530

    Update cardsData.js

---
+++ React/Practice/seller-app/src/components/cardsData.js
@@ -0,0 +1,62 @@
+const cardsData = {
+    "success":true,
+    "data": {
+        "hotel":[
+            {
+                "id" :1,
+                "type" : "hotel",
+                "name" : "Motif New",
+                "availability":"Rent",                
+                "address1" : "1415 5th Ave",
+                "rooms":3,
+                "beds":4,
+                "baths":1,
+                "area":732, //in sqft               
+                "city" : "newyork",
+                "confidenceRating" : 52,
+                "countryCode" : "US",
+            },
+            {
+                "id" :2,
+                "type" : "hotel",
+                "name" : "Taj Hotel",
+                "availability":"Rent",                
+                "address1" : "Mumbai street",
+                "rooms":3,
+                "beds":4,
+                "baths":1,
+                "area":732, //in sqft               
+                "city" : "mumbai",
+                "confidenceRating" : 52,
+                "countryCode" : "IN",
+            },
+            {
+                "id" :3,
+                "type" : "hotel",
+                "name" : "Motif New",
+                "availability":"Rent",                
+                "address1" : "1415 5th Ave",
+                "rooms":3,
+                "beds":4,
+                "baths":1,
+                "area":732, //in sqft               
+                "city" : "newyork",
+                "confidenceRating" : 52,
+                "countryCode" : "US",
+            },
+            {
+                "id" :4,
+                "type" : "hotel",
+                "name" : "Motif New",
+                "availability":"Rent",                
+                "address1" : "1415 5th Ave",
+                "rooms":3,
+                "beds":4,
+                "baths":1,
+                "area":732, //in sqft               
+                "city" : "newyork",
+                "confidenceRating" : 52,
+                "countryCode" : "US",
+            },
+       ] }
+    }
